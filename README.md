fc495fa9cce0714672f2e58546f4ff2c3c25a780
Bertrand Delacretaz <bdelacretaz@apache.org>
2022-09-29 12:04:01 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Testing Okteto
+
+Deploy a basic Node.js service, based on the
+https://medium.com/geekculture/deploy-api-on-kubernetes-for-free-ffe6889aad9d example.
---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const os = require("os");
+const app = express();
+const fs = require("fs");
+const path = require("path");
+const port = 8080;
+const loadTime = new Date();
+
+app.get("/", (_req, res) => {
+  res.send({
+    name: os.hostname(),
+    type: os.type(),
+    uptime: os.uptime(),
+    loadTime
+  })
+});
+
+app.get("/data", (_req, res) => {
+  fs.readFile(
+    path.join(__dirname, "/data.txt"),
+    { encoding: "utf-8" },
+    (err, f) => {
+      if (err) console.log(err);
+
+      res.setHeader("Access-Control-Allow-Origin", "*");
+      res.setHeader("Access-Control-Allow-Methods", "POST, GET");
+      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+      res.send(f);
+    }
+  );
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
